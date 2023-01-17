516e07431e26bbd54bc476da5f48c137dbf8222d
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-16 21:26:12 -0300

    0001 - Olá mundo

---
+++ src/main/java/github/jfrts/introducao/A0001_OlaMundo.java
@@ -0,0 +1,7 @@
+package github.jfrts.introducao;
+
+public class A0001_OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hello world...");
+    }
+}
---
+++ src/main/java/github/jfrts/introducao/A0002_TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package github.jfrts.introducao;
+
+public class A0002_TiposPrimitivos {
+
+    public static void main(String[] args) {
+        // Tipos primitivos vão guardar em memória um valor simples.
+        // int, double, float, char, byte, short, long, boolean
+
+        int idade = 10;
+        System.out.println(idade);
+
+    }
+
+}
