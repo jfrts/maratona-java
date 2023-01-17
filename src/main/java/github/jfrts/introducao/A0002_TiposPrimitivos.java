2d71336e2e5de664fb42ef91742a8067be9b10e8
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-16 23:35:56 -0300

    0002 - Tipos Primitivos pt3

--- src/main/java/github/jfrts/introducao/A0002_TiposPrimitivos.java
+++ src/main/java/github/jfrts/introducao/A0002_TiposPrimitivos.java
@@ -9,10 +9,12 @@ public class A0002_TiposPrimitivos {
         byte idade = 10;
         System.out.println("A idade é: " + idade);
 
+        long numeroGrande = 1000000L;
+        int numeroGrandeCasting = (int) 10000000L;
         double salarioDouble = 2000;
-        float salarioFloat = 2500F;
+        float salarioFloat = 2500.0F;
         byte idadeByte = 10;
-        short idadeShort = 10;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 'A';
