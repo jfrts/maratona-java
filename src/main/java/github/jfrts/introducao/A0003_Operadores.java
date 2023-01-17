4b77e8bd382aa52e133d7b4ff5e289c513ab09ec
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-17 00:02:53 -0300

    0003 - Operadores pt1

---
+++ src/main/java/github/jfrts/introducao/A0003_Operadores.java
@@ -0,0 +1,19 @@
+package github.jfrts.introducao;
+
+public class A0003_Operadores {
+
+    public static void main(String[] args) {
+        int primeiroNumero = 10;
+        int segundoNumero = 20;
+
+        System.out.println(primeiroNumero + segundoNumero); // 10
+        System.out.println("Valor: " + primeiroNumero + segundoNumero); // Valor: 1020
+        System.out.println(primeiroNumero + segundoNumero + "Valor: "); // 1020Valor:
+
+        // Operadores normais
+        /**
+         * [/, *, %...]
+         */
+    }
+
+}
