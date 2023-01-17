a18589eb8b61288baba2023d2015553f8a7c028e
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-16 21:40:48 -0300

    0002 - Tipos Primitivos pt2

--- src/main/java/github/jfrts/introducao/A0002_TiposPrimitivos.java
+++ src/main/java/github/jfrts/introducao/A0002_TiposPrimitivos.java
@@ -6,8 +6,16 @@ public class A0002_TiposPrimitivos {
         // Tipos primitivos vão guardar em memória um valor simples.
         // int, double, float, char, byte, short, long, boolean
 
-        int idade = 10;
-        System.out.println(idade);
+        byte idade = 10;
+        System.out.println("A idade é: " + idade);
+
+        double salarioDouble = 2000;
+        float salarioFloat = 2500F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
 
     }
 
