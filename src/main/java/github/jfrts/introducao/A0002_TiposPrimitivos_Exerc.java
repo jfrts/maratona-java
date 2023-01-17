156a7a380fa105100163d924113cf1976fcd0ce0
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-16 23:56:43 -0300

    0002 - Tipos Primitivos pt5exerc

---
+++ src/main/java/github/jfrts/introducao/A0002_TiposPrimitivos_Exerc.java
@@ -0,0 +1,22 @@
+package github.jfrts.introducao;
+
+/**
+ * Prática:
+ *
+ * Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+ *
+ * Eu <nome>, morando no endereço <endereco>, confirmo que recebi o salário de <salario>, na data <data>
+ */
+
+public class A0002_TiposPrimitivos_Exerc {
+    public static void main(String[] args) {
+        String nome = "Júlio";
+        String endereco = "Rua ..., 0, Centro";
+        double salario = 3500;
+        String data = "16/01/2023";
+
+        System.out.printf("Eu %s, morando no endereço %s, confirmo que recebi o salário de R$ %.2f, na data %s.",
+                nome, endereco, salario, data);
+    }
+
+}
