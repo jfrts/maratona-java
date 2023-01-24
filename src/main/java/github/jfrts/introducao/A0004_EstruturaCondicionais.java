df4a4a01affe4075b0fdcf3d31a60fef07d6133d
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-23 21:28:05 -0300

    0004 - Estruturas Condicionais

---
+++ src/main/java/github/jfrts/introducao/A0004_EstruturaCondicionais.java
@@ -0,0 +1,23 @@
+package github.jfrts.introducao;
+
+public class A0004_EstruturaCondicionais {
+
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("Dentro do if");
+        }
+
+        System.out.println("Fora do if");
+
+        int idade = 20;
+
+        if (idade > 20) {
+            System.out.println("Autorizado a comprar bebida alcoólica");
+        } else {
+            System.out.println("Não Autorizado a comprar bebida alcoólica");
+        }
+
+        
+    }
+
+}
